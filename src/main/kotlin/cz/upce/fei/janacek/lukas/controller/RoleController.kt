fcf71d630e84f5a25d3d2f1fc93130a9c3eebacd
Lukáš Janáček <ljanacek@netvoice.cz>
2023-03-18 21:38:30 +0100

    Added controllers to model.

---
+++ src/main/kotlin/cz/upce/fei/janacek/lukas/controller/RoleController.kt
@@ -0,0 +1,58 @@
+package cz.upce.fei.janacek.lukas.controller
+
+import cz.upce.fei.janacek.lukas.dto.RoleExternalDto
+import cz.upce.fei.janacek.lukas.dto.toEntity
+import cz.upce.fei.janacek.lukas.dto.toExternalDto
+import cz.upce.fei.janacek.lukas.service.RoleService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@Controller
+@RequestMapping("/roles/")
+class RoleController (
+    private val roleService: RoleService
+) {
+
+    @GetMapping("/{id}")
+    fun getRoleById(
+        @PathVariable
+        id: Long
+    ): ResponseEntity<RoleExternalDto> {
+        val role = roleService.findById(id)
+        return ResponseEntity.ok(role.toExternalDto())
+    }
+
+    @PostMapping("")
+    fun create(
+        @RequestBody
+        @Validated
+        roleDto: RoleExternalDto
+    ): ResponseEntity<RoleExternalDto> {
+        val result = roleService.create(roleDto.toEntity())
+        return ResponseEntity<RoleExternalDto>(result.toExternalDto(), HttpStatus.CREATED)
+    }
+
+    @PutMapping("/{id}")
+    fun modify(
+        @PathVariable
+        id: Long,
+        @RequestBody
+        @Validated
+        roleDto: RoleExternalDto
+    ): ResponseEntity<RoleExternalDto> {
+        val result = roleService.modify(id, roleDto.toEntity(id))
+        return ResponseEntity<RoleExternalDto>(result.toExternalDto(), HttpStatus.ACCEPTED)
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(
+        @PathVariable
+        id: Long
+    ): ResponseEntity<RoleExternalDto> {
+        val result = roleService.delete(id)
+        return ResponseEntity<RoleExternalDto>(result.toExternalDto(), HttpStatus.FOUND)
+    }
+}
---
+++ src/main/kotlin/cz/upce/fei/janacek/lukas/controller/TeamController.kt
@@ -0,0 +1,58 @@
+package cz.upce.fei.janacek.lukas.controller
+
+import cz.upce.fei.janacek.lukas.dto.TeamExternalDto
+import cz.upce.fei.janacek.lukas.dto.toEntity
+import cz.upce.fei.janacek.lukas.dto.toExternalDto
+import cz.upce.fei.janacek.lukas.service.TeamService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@Controller
+@RequestMapping("/teams/")
+class TeamController (
+    private val teamService: TeamService
+) {
+
+    @GetMapping("/{id}")
+    fun getTeamById(
+        @PathVariable
+        id: Long
+    ): ResponseEntity<TeamExternalDto> {
+        val team = teamService.findById(id)
+        return ResponseEntity.ok(team.toExternalDto())
+    }
+
+    @PostMapping("")
+    fun create(
+        @RequestBody
+        @Validated
+        teamDto: TeamExternalDto
+    ): ResponseEntity<TeamExternalDto> {
+        val result = teamService.create(teamDto.toEntity())
+        return ResponseEntity<TeamExternalDto>(result.toExternalDto(), HttpStatus.CREATED)
+    }
+
+    @PutMapping("/{id}")
+    fun modify(
+        @PathVariable
+        id: Long,
+        @RequestBody
+        @Validated
+        teamDto: TeamExternalDto
+    ): ResponseEntity<TeamExternalDto> {
+        val result = teamService.modify(id, teamDto.toEntity(id))
+        return ResponseEntity<TeamExternalDto>(result.toExternalDto(), HttpStatus.ACCEPTED)
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(
+        @PathVariable
+        id: Long
+    ): ResponseEntity<TeamExternalDto> {
+        val result = teamService.delete(id)
+        return ResponseEntity<TeamExternalDto>(result.toExternalDto(), HttpStatus.FOUND)
+    }
+}
