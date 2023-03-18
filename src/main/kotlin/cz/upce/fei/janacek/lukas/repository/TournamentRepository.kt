4e7f0368614d091e082fa10a2cba964e44c6245c
Lukáš Janáček <ljanacek@netvoice.cz>
2023-03-18 22:02:18 +0100

    Added missing @Repository annotations.

--- src/main/kotlin/cz/upce/fei/janacek/lukas/repository/TournamentRepository.kt
+++ src/main/kotlin/cz/upce/fei/janacek/lukas/repository/TournamentRepository.kt
@@ -2,7 +2,9 @@ package cz.upce.fei.janacek.lukas.repository
 
 import cz.upce.fei.janacek.lukas.model.Tournament
 import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface TournamentRepository: PagingAndSortingRepository<Tournament, Long> {
 
 }
--- src/main/kotlin/cz/upce/fei/janacek/lukas/repository/UserRepository.kt
+++ src/main/kotlin/cz/upce/fei/janacek/lukas/repository/UserRepository.kt
@@ -2,7 +2,9 @@ package cz.upce.fei.janacek.lukas.repository
 
 import cz.upce.fei.janacek.lukas.model.User
 import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository: PagingAndSortingRepository<User, Long> {
 
 }
